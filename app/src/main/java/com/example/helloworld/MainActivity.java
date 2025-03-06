49f55ee9daf6a7f69554a66d1b929b73c05cb3d7
maxclein <22364862@studentmail.ul.ie>
2025-03-06 15:28:54 +0000

    updated calendar

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,16 +29,26 @@ public class MainActivity extends AppCompatActivity implements CalenderActivity.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button calendarButton = findViewById(R.id.calendarButton);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalenderActivity.class);
+                startActivity(intent);
+            }
+        });
+
         initWidgets();
         selectedDate = LocalDate.now();
         setMonthView();
     }
-
 
     private void initWidgets() {
         calendarRecyclerView = findViewById(R.id.calendarRecycleView);
