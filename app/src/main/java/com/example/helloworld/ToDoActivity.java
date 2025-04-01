41bfea06c24045f135a1e555ce1c71c784d9bcc0
raidmouras <22368566@studentmail.ul.ie>
2025-04-01 16:08:41 +0100

    created a To-do activity included add buttons and edit text section Added database todo fully complete

--- app/src/main/java/com/example/helloworld/ToDoActivity.java
+++ app/src/main/java/com/example/helloworld/ToDoActivity.java
@@ -1,14 +1,35 @@
 package com.example.helloworld;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class ToDoActivity extends AppCompatActivity {
+import com.example.helloworld.Model.ToDoModel;
+import com.example.helloworld.Utils.ToDoDatabaseHelper;
+import com.example.helloworld.toDoAdapter.ToDoAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ToDoActivity extends AppCompatActivity implements ToDoOnDialogCloseListener{
+
+    RecyclerView recyclerViewToDo;
+    FloatingActionButton addToDoButton;
+    ToDoDatabaseHelper todoDb;
+    private List<ToDoModel> mList;
+    private ToDoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,41 @@ public class ToDoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerViewToDo = findViewById(R.id.recyclerViewToDo);
+        addToDoButton = findViewById(R.id.addToDoButton);
+        todoDb = new ToDoDatabaseHelper(ToDoActivity.this);
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(todoDb,ToDoActivity.this);
+
+        recyclerViewToDo.setHasFixedSize(true);
+        recyclerViewToDo.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewToDo.setAdapter(adapter);
+
+        mList= todoDb.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+
+
+
+
+        addToDoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewTask.newInstance().show(getSupportFragmentManager(),addNewTask.TAG);
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new toDoRViewTHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(recyclerViewToDo);
+    }
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList= todoDb.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+        adapter.notifyDataSetChanged();
+
     }
 }
---
+++ app/src/main/java/com/example/helloworld/toDoRViewTHelper.java
@@ -0,0 +1,68 @@
+package com.example.helloworld;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.helloworld.toDoAdapter.ToDoAdapter;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
+public class toDoRViewTHelper extends ItemTouchHelper.SimpleCallback {
+
+    ToDoAdapter adapter;
+
+    public toDoRViewTHelper(ToDoAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getAdapterPosition();
+        if (direction == ItemTouchHelper.LEFT) {
+            AlertDialog.Builder builder= new AlertDialog.Builder(adapter.getContext());
+            builder.setTitle("Delete Task");
+            builder.setMessage("Are you Sure");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    adapter.deleteTask(position);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    adapter.notifyItemChanged(position);
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else {
+            adapter.editItem(position);
+        }
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.red))
+                .addSwipeLeftActionIcon(R.drawable.tempdelete)
+                .addSwipeRightBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.green))
+                .addSwipeRightActionIcon(R.drawable.tempedit)
+                .create()
+                .decorate();
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+}
